16435b5b1f38e3c1ca2b4191dbee55fcc20f6685
shaaaaaaq <42686442+shaaaaaaq@users.noreply.github.com>
2018-08-26 04:15:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -341,5 +341,27 @@ client.on('message', message => {
       message.channel.send(helpEmbed);
     }
 });
+client.on("message", msg => {
+  if(msg.content === '$' + "id") {
+      const embed = new Discord.RichEmbed();
+  embed.addField("🔱| اسم الحساب :", `${msg.author.username}#${msg.author.discriminator}`, true)
+          .addField("🆔| الاي دي :", `${msg.author.id}`, true)
+          .setColor("RANDOM")
+          .setFooter(msg.author.username , msg.author.avatarURL)
+          .setThumbnail(`${msg.author.avatarURL}`)
+          .setTimestamp()
+          .setURL(`${msg.author.avatarURL}`)
+          .addField('📛| الحالة :', `${msg.author.presence.status.toUpperCase()}`, true)
+          .addField('🎲| بلاينج :', `${msg.author.presence.game === null ? "No Game" : msg.author.presence.game.name}`, true)
+          .addField('🏅| الرتب : ', `${msg.member.roles.filter(r => r.name).size}`, true)
+          .addField('📅| تم الانضمام للديسكورد في :', `${msg.createdAt}`,true)
+          .addField('🤖| هل هو بوت ؟', `${msg.author.bot.toString().toUpperCase()}`, true);
+      msg.channel.send({embed: embed})
+  }
+});
+
+
+
 
 client.login(process.env.BOT_TOKEN);
+

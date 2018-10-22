5cfe593b9cdf2948536e049d5e794a8e44df1219
shaaaaaaq <42686442+shaaaaaaq@users.noreply.github.com>
2018-10-22 13:17:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -385,23 +385,7 @@ let args = message.content.split(' ').slice(1).join(' ');
         
 }
 });
-client.on('ready', function(){    
-    var ms = 40000 ;    
-    var setGame = ['Abdullah#0110'];    
-    var i = -1;    
-    var j = 0;    
-    setInterval(function (){    
-        if( i == -1 ){    
-j = 1;    
-       }    
-        if( i == (setGame.length)-1 ){    
-            j = -1;    
-      }    
-       i = i+j;    
-        client.user.setGame(setGame[i],`http://www.youtube.com/gg`);    
-}, ms);    
-    
-});
+
 
 
 client.login(process.env.BOT_TOKEN);
